e1a2456b65cf4bc6077f101729222fe06073b1f3
Olga Yarosh <O_ll_y_a@mail.ru>
2023-04-23 17:30:40 +0300

    HW1 done

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employer
+(
+    first_name varchar(100) NOT NULL,
+	last_name varchar(100),
+	title varchar(100),
+	birth_date varchar(100),
+	notes text
+);
+
+CREATE TABLE customers
+(
+    customer_id int PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+
+CREATE TABLE orders
+(
+    order_id int,
+	customer_id int PRIMARY KEY,
+	employee_id int,
+	order_date varchar(100),
+	ship_city varchar(100)
+)
